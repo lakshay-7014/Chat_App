e3153d25f30afa3f521c5640be2ce6fb5500189b
Lakshay <lakshayagarwal298895@gmail.com>
2022-05-08 16:55:25 +0530

    This is a commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_app/screens/welcome_screen.dart';
 import 'package:chat_app/screens/login_screen.dart';
@@ -6,7 +7,8 @@ import 'package:chat_app/screens/chat_screen.dart';
 
 
 void main() async {
-
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
