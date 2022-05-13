c64ccb10fa41dbf5f522acc5e40c39eefa6c320d
Lakshay <lakshayagarwal298895@gmail.com>
2022-05-13 21:36:34 +0530

    This is a commit

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -13,31 +13,40 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  final _auth= FirebaseAuth.instance;
-  final _fireStore= FirebaseFirestore.instance;
+  final _auth = FirebaseAuth.instance;
+  final _fireStore =
+      FirebaseFirestore.instance; //instance to store data in firestore
   dynamic loggedInUser;
   late String messageText;
-
+  final TextEditingController controller = TextEditingController();
+  late Stream<QuerySnapshot> stream;
   @override
-  void initState(){
-
+  void initState() {
     super.initState();
     getCurrentUser();
+    getData();
   }
 
-
-  void getCurrentUser()async{
-    final user= await _auth.currentUser;
+  void getCurrentUser() async {
+    final user = await _auth.currentUser;
     try {
       if (user != null) {
         loggedInUser = user;
       }
-    }
-    catch(e){
+    } catch (e) {
       print(e);
     }
   }
 
+  getData() async {
+    // function to fetch data from firebase firestore
+    setState(() {});
+    stream = await _fireStore
+        .collection('messages')
+        .orderBy('ts', descending: true)
+        .snapshots();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,9 +57,9 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: const Icon(Icons.close),
               onPressed: () {
+                //Implement logout functionality
                 _auth.signOut();
                 Navigator.pop(context);
-                //Implement logout functionality
               }),
         ],
         title: const Text('⚡️Chat'),
@@ -61,27 +70,60 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot>(
+                // stream builder is used for continuous data stream
+                //Widget that builds itself based on the latest snapshot
+                stream: stream,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  {
+                    return ListView.builder(
+                        reverse: true,
+                        shrinkWrap: true,
+                        itemCount: snapshot.data?.docs.length,
+                        itemBuilder: (context, index) {
+                          DocumentSnapshot ds = snapshot.data!.docs[index];
+                          // print(ds['text']);
+                          return Row(
+                            children: [
+                              Expanded(
+                                child: Text(ds['text']),
+                              ),
+                            ],
+                          );
+                        });
+                  }
+                }),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   Expanded(
-                    child: TextField(
+                    child: TextFormField(
+                      controller: controller,
                       onChanged: (value) {
-                        messageText=value;
+                        messageText = value;
                         //Do something with the user input.
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {
+                    onPressed: () async {
+                      controller.clear();
                       //Implement send functionality.
-                      _fireStore.collection('messages').add({
-                        'text':messageText,
-                        'sender':loggedInUser.email,
+                      await _fireStore.collection('messages').add({
+                        'text': messageText,
+                        'sender': loggedInUser.email,
+                        'ts': DateTime.now(),
                       });
+                      await getData();
+                      setState(() {});
                     },
                     child: const Text(
                       'Send',
